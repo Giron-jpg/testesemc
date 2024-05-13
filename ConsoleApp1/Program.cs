eac7869b66780073857f623b2549ee5f397ea840
João Paulo Giron <joaopaulog964@gmail.com>
2024-05-13 17:24:38 -0300

    Testes 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.WriteLine("Hellow World");
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Hellow World");
 string name = Console.ReadLine();
 Console.WriteLine("Bem vindo {0}", name);
+int a = 4;
+int b = 3;
+int c = a + b;
+
+var testetupla = new List
+
+for(int i = 0; i < 3; i++){
+    Console.WriteLine((i));
+}
+
+Console.WriteLine(c);
 Console.ReadLine();
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03215a63167d9e2dcf385b7ba66607e357e1cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3aaefad0d11482b27fd498998061616bbb71b3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
