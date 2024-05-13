b3aaefad0d11482b27fd498998061616bbb71b3f
João Paulo Giron <joaopaulog964@gmail.com>
2024-05-13 16:49:28 -0300

    Testes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hellow World");
+string name = Console.ReadLine();
+Console.WriteLine("Bem vindo {0}", name);
+Console.ReadLine();
--- olamundo.cs
+++
@@ -1,1 +0,0 @@
-Console.Writeline("Olá Mundo");
